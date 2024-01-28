d44bced50c12cac3cd46243e0ccfcb5d38c3f01c
Okello Sam <samuelokello929@gmail.com>
2024-01-28 14:55:32 +0300

    Added functionality to handle configuration changes in MainViewModel.kt

--- app/src/main/java/com/samuelokello/counter/MainViewModel.kt
+++ app/src/main/java/com/samuelokello/counter/MainViewModel.kt
@@ -6,20 +6,28 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
-    private val _count = MutableStateFlow(0)
+    private val _count = MutableStateFlow(State())
     val count = _count.asStateFlow()
 
     private val MAX_COUNT = 110
 
     fun incrementCount() {
-        val currentCount = _count.value ?: 0
-        _count.value = currentCount + 1
+        if (_count.value.count >= MAX_COUNT) return
+        else {
+            val currentCount = _count.value
+            _count.value = State(currentCount.count + 1)
+        }
     }
 
     fun decrementCount() {
-        val currentCount = _count.value ?: 0
-        if (currentCount > 0) {
-            _count.value = currentCount - 1
+        if (_count.value.count <= 0) return
+        else {
+            val currentCount = _count.value
+            _count.value = State(currentCount.count - 1)
         }
     }
 }
+
+data class State(
+    val count: Int = 0
+)
